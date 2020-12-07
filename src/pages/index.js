bf96d4c31b951c0a0834818eb14d51b6f51b9d15
Albert Kenneth Nguyen <albert@sweepenergy.com>
2020-12-07 07:53:14 -0800

    Move files into src folder

---
+++ src/pages/index.js
@@ -0,0 +1,11 @@
+import Head from 'next/head'
+import styles from '../styles/Home.module.css'
+import 'semantic-ui-css/semantic.min.css'
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+
+    </div>
+  )
+}
