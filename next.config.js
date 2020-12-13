106ee38136e6cadde9aa0801fd5f06bcf400ffa1
Albert Kenneth Nguyen <albert@sweepenergy.com>
2020-12-13 06:07:32 -0800

    expose environmental variables

---
+++ next.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  env: {
+    DB_NAME: process.env.DB_NAME,
+    DB_USER: process.env.DB_USER,
+    DB_PASS: process.env.DB_PASS,
+    MAPBOX_KEY: process.env.MAPBOX_KEY,
+    GOOGLE_KEY: process.env.GOOGLE_KEY
+  },
+}
