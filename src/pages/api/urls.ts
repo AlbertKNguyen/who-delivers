4dc219c305855fbf48aab61928583532a2f68a7e
Albert Kenneth Nguyen <albert@sweepenergy.com>
2020-12-18 22:07:32 -0800

    Change to snake_case, add headers, start db code

--- src/pages/api/urls.ts
+++ src/pages/api/urls.ts
@@ -1,38 +1,84 @@
-const cheerio = require('cheerio');
-const axios = require('axios').default;
+import cheerio from 'cheerio';
+import axios from 'axios';
 
-export default async (req, res) => {
+let client;
+
+interface Request {
+  method: string;
+  query: {
+    place_id: string;
+    search: string;
+  };
+}
+
+const config = {
+  headers: {
+    'User-Agent':
+      'Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html',
+  },
+};
+
+export default async (req: Request, res) => {
   if (req.method === 'GET') {
-    const url = `https://www.google.com/search?hl=en&q=${req.query.search}`;
+    try {
+      const url = `https://www.google.com/search?hl=en&q=${req.query.search}`;
+      let data;
+      data = (await axios.get(url, config)).data;
+      const $ = cheerio.load(data);
 
-    const { data } = await axios.get(url);
-    const $ = cheerio.load(data);
+      let url_list: string[] = [];
+      const links = $('a');
+      $($('a')).each((i, link) => {
+        const link_title = $(link).text();
 
-    let urlList = [];
-    const links = $('a');
-    $($('a')).each((i, link) => {
-      const linkTitle = $(link).text();
+        if (
+          link_title.includes('Delivery') ||
+          link_title.includes('Order') ||
+          link_title.includes('Website')
+        ) {
+          let website_url: string;
+          const href = $(link).attr('href');
 
-      if (linkTitle.includes('Delivery') || linkTitle.includes('Order') || linkTitle.includes('Website')) {
-        let websiteURL: string;
-        const href = $(link).attr('href');
+          if (!href.includes('yelp') && href.includes('http')) {
+            if (href.startsWith('http')) {
+              website_url = href;
+            } else {
+              website_url = href.substr(
+                7,
+                $(link).attr('href').indexOf('&sa') - 7
+              );
+            }
 
-        if (!href.includes('yelp') && href.includes('http')) {
-          if (href.startsWith('http')) {
-            websiteURL = href;
-          } else {
-            websiteURL = href.substr(
-              7,
-              $(link).attr('href').indexOf('&sa') - 7
-            );
+            url_list = Array.from(new Set([...url_list, website_url]));
           }
-
-          urlList = Array.from(new Set([...urlList, websiteURL]));
         }
-      }
-    });
-    res.status(200).json({ urls: urlList });
+      });
+
+      res.status(200).json({ urls: url_list });
+    } catch {
+      res.status(500).json({ message: 'Error when retrieving data' });
+    }
   } else {
-    res.sendStatus(400);
+    res.status(400).json({ status: 'invalid_request_type' });
   }
+};
+
+const findURLsDocument = async (place_id: string) => {
+  let urls: string[] = [];
+  const query = { place_id: place_id };
+  const collection = client.db('restaurants').collection('urls');
+
+  const data = await collection.findOne(query);
+  if (data !== null) {
+    urls = data.urls;
+  }
+
+  return urls;
+};
+
+const insertURLsDocument = async (place_id: string, url_list: string[]) => {
+  const collection = client.db('restaurants').collection('urls');
+
+  const document = { place_id: place_id, urls: url_list };
+  const result = await collection.insert(document);
 };
