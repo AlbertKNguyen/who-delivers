e7b3bcba4ac9febcdbe4af2ceb3b26d5e36d3903
Albert Kenneth Nguyen <albert@sweepenergy.com>
2020-12-19 02:34:06 -0800

    Use yahoo, add search engines + filters,

--- src/pages/api/urls.ts
+++ src/pages/api/urls.ts
@@ -19,19 +19,26 @@ const config = {
   },
 };
 
+const search_engines = [
+  'https://www.google.com/search?hl=en&q=',
+  'https://search.yahoo.com/search?p=',
+  'https://www.bing.com/search?q=',
+  'https://www.bing.com/maps?q=',
+];
+
 export default async (req: Request, res) => {
   if (req.query.key === process.env.SECRET_KEY) {
     if (req.method === 'GET') {
       try {
-        const url = `https://www.google.com/search?hl=en&q=${req.query.search}`;
+        const url = `${search_engines[1]}${req.query.search}&atb=v177-1&ia=web`;
         const { data } = await axios.get(url, config);
         const $ = cheerio.load(data);
 
         let url_list: string[] = [];
         const links = $('a');
+
         $($('a')).each((i, link) => {
           const link_title = $(link).text();
-
           if (
             link_title.includes('Delivery') ||
             link_title.includes('Order') ||
@@ -40,7 +47,13 @@ export default async (req: Request, res) => {
             let website_url: string;
             const href = $(link).attr('href');
 
-            if (!href.includes('yelp') && href.includes('http')) {
+            if (
+              !href.includes('yelp') &&
+              !href.includes('yahoo') &&
+              !href.includes('bing') &&
+              !href.includes('mapquest') &&
+              href.includes('http')
+            ) {
               if (href.startsWith('http')) {
                 website_url = href;
               } else {
@@ -54,7 +67,6 @@ export default async (req: Request, res) => {
             }
           }
         });
-
         res.status(200).json({ urls: url_list });
       } catch (error) {
         console.log(error);
